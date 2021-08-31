5d31e24d6fa1ee9dff7a5174f84b2358ffa6b895
cpuu <cpuu@icloud.com>
2021-08-31 23:02:05 +0900

    Complete ex7

--- Matlab/ex7/findClosestCentroids.m
+++ Matlab/ex7/findClosestCentroids.m
@@ -9,7 +9,7 @@ function idx = findClosestCentroids(X, centroids)
 K = size(centroids, 1);
 
 % You need to return the following variables correctly.
-idx = zeros(size(X,1), 1);
+idx = ones(size(X,1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Go over every example, find its closest centroid, and store
@@ -19,15 +19,18 @@ idx = zeros(size(X,1), 1);
 %               range 1..K
 %
 % Note: You can use a for-loop over the examples to compute this.
-%
 
+calc = zeros(K,1);
 
-
-
-
-
+% ugly code detected!!
+for i=1:size(X,1),
+	for k=1:K,
+		calc(k) = sum((X(i,:) - centroids(k,:)).^2);	
+    end;
+    [val,index] = min(calc);
+    idx(i) = index;
+end;
 
 % =============================================================
 
 end
-
